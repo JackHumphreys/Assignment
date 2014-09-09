3f8ecefec3fa2754f31cf1ce2a8c68760d2ed3fd
40618 <40618@longroad.ac.uk>
2014-09-09 12:56:37 +0100

    Variable change

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,8 @@
-Python 3.4.1 (v3.4.1:c0e311e010fc, May 18 2014, 10:38:22) [MSC v.1600 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> print ("hello world")
+#Jack Humphreys
+#09/09/2014
+#Hello World Exercise
+
+print ("Hello World")
+first_name = input("Enter your name:")
+print (first_name)
+print ("Hi {0}".format(first_name)) 
