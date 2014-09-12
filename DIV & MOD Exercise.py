a29dbf0ed053f2642990da81f5427baca0e244e8
40618 <40618@longroad.ac.uk>
2014-09-12 14:30:01 +0100

    Division and remainders exercise

---
+++ DIV & MOD Exercise.py
@@ -0,0 +1,9 @@
+#Jack Humphreys
+#12/09/2012
+#DIV & MOD Exercise
+
+number1 = int(input("Please enter first number: "))
+number2 = int(input("Please enter second number: "))
+
+print("Times first number divide second number: {0}".format(number1//number2))
+print("Remainder of first number divided by second number: {0}".format(number1%number2))
