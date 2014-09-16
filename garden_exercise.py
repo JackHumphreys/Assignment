746a74a65cfdc2c2e13210a37ce1c06a330ea526
40618 <40618@longroad.ac.uk>
2014-09-16 12:58:33 +0100

    Stretch & challenge garden exercise

---
+++ garden_exercise.py
@@ -0,0 +1,10 @@
+#Jack Humphreys
+#Garden Exercise
+#16/09/2014
+
+length = float("Please enter length in metres: ")
+width = float("Please enter width in metres: ")
+
+area = float(length*width)
+
+cost = float(area*10)
