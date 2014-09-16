dda8c61f626319f9aaf11e5f44bfac0d9e09e07a
40618 <40618@longroad.ac.uk>
2014-09-16 12:28:55 +0100

    Temperature exercise

--- temperature_exercise.py
+++ temperature_exercise.py
@@ -1,2 +1,6 @@
-fahrenheit = int(input("Please enter temperature in farenheit: "))
-print("Temperature in
+#Jack Humphreys
+#16/09/2014
+#Fahrenheit to Centigrade Exercise
+fahrenheit = float(input("Please enter temperature in farenheit: "))
+centigrade = float(fahrenheit-32)*(5/7)
+print("Temperature in Centigrade: {0}".format (centigrade))
