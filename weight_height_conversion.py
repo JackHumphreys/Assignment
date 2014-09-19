165c3183e9e819a3b772260e28ff9ac8e3ebb155
40618 <40618@longroad.ac.uk>
2014-09-19 14:31:02 +0100

    Weight & Height conversion.

--- weight_height_conversion.py
+++ weight_height_conversion.py
@@ -1,5 +1,6 @@
-weight = float(input("Please enter weight: "))
-height = float(input("Please enter height: "))
 
-print("Your weight in kilograms is: {0}".format(weight*6.364))
-print("Your height in centimetre is: {0}".format(height*2.54))
+height_1 = float(input("Please enter height in inches: "))
+weight_1 = float(input("Please enter weight in stone: "))
+print("Your weight in kilograms is: {0}".format(weight_1*6.364))
+print("Your height in centimetre is: {0}".format(height_1*2.54))
+
