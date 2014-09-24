d80da778a69e5de7786fdd244d42fb83c3d20c7a
Jack <40618@longroad.ac.uk>
2014-09-24 23:14:50 +0100

    Range exercise.

---
+++ range_exercise.py
@@ -0,0 +1,8 @@
+number = int(input("Please enter a number: "))
+if number > 20:
+             print("Number not within range and is too high.")
+elif number < 1:
+             print("Number not within range and is too low.")
+elif number > 1 <20:
+             print("Your number is in range.")
+    
